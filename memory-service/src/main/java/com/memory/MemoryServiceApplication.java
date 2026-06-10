46c56c05c85646a078539190f2d3bdc0237c67fe
Abhishek Kumar <abhishek9304302981@gmail.com>
2026-06-10 22:21:36 +0530

    Initial commit

---
+++ memory-service/src/main/java/com/memory/MemoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.memory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryServiceApplication.class, args);
+	}
+
+}
